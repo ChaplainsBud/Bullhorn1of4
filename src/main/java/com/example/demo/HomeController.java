8e8ecda3a392a79d93c672cda44781159daade8b
RoutinePoutine <matthew.robert.leroe@gmail.com>
2019-12-13 15:40:01 -0500

    Added Cloudinary Photo

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -4,12 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.ObjectUtils;
 
 import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HomeController {
@@ -17,18 +18,43 @@ public class HomeController {
     @Autowired
     MessageRepository messageRepository;
 
+    @Autowired
+    ImageRepository imageRepository;
+
+    @Autowired
+    CloudinaryConfig cloudc;
+
     @RequestMapping("/")
     public String homePage(Model model){
         model.addAttribute("messages", messageRepository.findAll());
+        model.addAttribute("images", imageRepository.findAll());
         return "list";
     }
 
     @GetMapping("/add")
     public String messageForm(Model model){
         model.addAttribute("message", new Message());
+        model.addAttribute("image", new Image());
         return "messageform";
     }
-
+/*
+    @PostMapping("/add")
+    public String processImage(@ModelAttribute Image image,
+                               @RequestParam("file")MultipartFile file){
+        if(file.isEmpty()){
+            return "redirect:/add";
+        }
+        try {
+            Map uploadResult = cloudc.upload(file.getBytes(),
+                   ObjectUtils.asMap("resourcetype", "auto"));
+            image.setHeadshot(uploadResult.get("url").toString());
+            imageRepository.save(image);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "redirect:/add";
+        }
+    }
+*/
     @PostMapping("/process")
     public String processForm(@Valid Message message,
                               BindingResult result){
